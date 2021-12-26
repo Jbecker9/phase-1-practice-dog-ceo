a3fb79f5b76e4543a49eaf5c9ff16e983a93141d
Jared <jbeckersalax9@aol.com>
2021-12-26 16:18:38 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 console.log('%c HI', 'color: firebrick')
 
-fetch("https://dog.ceo/api/breeds/image/random/4")
-.then(response => response.json())
-.then(() => {
-    const dogImages = document.getElementById("dog-image-container")
-    dogImages.appendChild()
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("https://dog.ceo/api/breeds/image/random/4").then(response => response.json()).then(data => data.message.forEach(element => {
+        imageAdd(element)
+    }))
 })
+
+function imageAdd(url){
+    const dogImage = document.getElementById("dog-image-container")
+    const img = dogImage.createElement('img')
+    dogImage.append(img)
+}
