8d08b407f2aaa5fde79911780fbf1419c919e86c
Jared <jbeckersalax9@aol.com>
2021-12-29 17:46:28 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ console.log('%c HI', 'color: firebrick')
 document.addEventListener("DOMContentLoaded", () => {
     fetchDogs()
     fetchBreeds()
-    const breedDropdown = document.getElementById("breed-dropdown")
-    breedDropdown.addEventListener("change", filter)
+    // filter()
 })
 
 function imageAdd(url){
@@ -42,7 +41,7 @@ function listAdd(breed){
         // return color = li.style.color = "#FF0000"
         // }
         // else if(li.style = "#FF0000"){
-        // return color = li.style.color = "#000000"
+        // return color = li.style.color = "#000000"`
         // }
     })
 }
@@ -52,21 +51,21 @@ function fetchDogs(){
         imageAdd(element)
     }))
 }
-
+let allBreeds = []
 function fetchBreeds(){
     fetch('https://dog.ceo/api/breeds/list/all').then(response => response.json()).then(data => {
-    breeds = Object.keys(data.message)
-    breeds.forEach(element => listAdd(element))
+    allBreeds = [...Object.keys(data.message)]
+    console.log(allBreeds)
+    allBreeds.forEach(element => {
+        listAdd(element)
     })
-}
-
-function filter(){
-    const li = document.getElementsByClassName("dog-list")
-    const breedDropdown = document.getElementById("breed-dropdown")
-    breedDropdown.addEventListener("change", () => {
-        switch(breedDropdown[value]){
-            case "a": forEach(value.letter)
-        }
-            
     })
+    // function filter(){
+        const li = document.getElementsByClassName("dog-list")
+        const selectorBreeds = document.querySelectorAll(".dog-list")
+        const breedDropdown = document.getElementById("breed-dropdown")
+        breedDropdown.addEventListener("change", (e) => {
+            allBreeds.filter(breed => breed[0] === e.target.value).then(allBreeds.splice(0, allBreeds.length - 1, breed))
+        })
+    // }
 }
